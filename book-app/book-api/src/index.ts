7cc23be3980788c498819b2938a29168058e2860
Yuliia-fed23 <147343499+Yuliia-fed23@users.noreply.github.com>
2025-05-08 20:05:47 +0200

    Connect to MongoDB

---
+++ book-app/book-api/src/index.ts
@@ -0,0 +1,22 @@
+import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+
+
+dotenv.config();
+
+const app = express();
+app.use(express.json()); // Parse JSON request
+
+// Connect to MongoDB
+mongoose.connect(process.env.MONGO_URI as string)
+  .then(() => console.log('Connected to MongoDB'))
+  .catch(err => console.error('MongoDB connection error:', err));
+
+
+
+const PORT = 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
