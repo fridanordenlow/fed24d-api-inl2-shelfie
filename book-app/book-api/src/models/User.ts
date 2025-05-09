a04451c6bc8deda8fabbf2900f801e6ec1427a54
Frida Nordenlöw <frida.nordenlow@hotmail.com>
2025-05-09 11:44:59 +0200

    feat(user-model): create UserSchema for user data model

---
+++ book-app/book-api/src/models/User.ts
@@ -0,0 +1,23 @@
+import mongoose, { Schema } from 'mongoose';
+import { IUser } from '../types/IUser.js';
+
+const UserSchema: Schema = new Schema<IUser>({
+  username: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  is_admin: {
+    type: Boolean,
+    required: true,
+  },
+  created_at: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+export default mongoose.model<IUser>('User', UserSchema);
