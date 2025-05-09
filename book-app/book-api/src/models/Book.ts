c3d9d92989010689408623de32b33911d2130eb9
maria-jon <mariajonasson93@gmail.com>
2025-05-09 12:21:14 +0200

    add mongoose bookSchema

---
+++ book-app/book-api/src/models/Book.ts
@@ -0,0 +1,35 @@
+import mongoose from "mongoose";
+const Schema = mongoose.Schema;
+
+const BookSchema = new Schema({
+	title: {
+		type: String,
+		required: true
+	},
+	description: {
+		type: String,
+		required: true
+	},
+    author: {
+		type: String,
+		required: true
+	},
+    genres: {
+		type: [String],
+		required: true
+	},
+    image: {
+		type: String,
+		required: true
+	},
+	published_year: {
+		type: Number,
+		required: true
+	},
+    reviews: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Reviews' 
+      }]
+});
+ 
+export default mongoose.model('Books', BookSchema);
