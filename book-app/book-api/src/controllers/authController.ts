9efd12614b4d9cbc19c2c7be1eed3d9f0c66b72c
Frida Nordenlöw <frida.nordenlow@hotmail.com>
2025-05-10 11:52:43 +0200

    feat(auth-router): complete auth router with login and logout

--- book-app/book-api/src/controllers/authController.ts
+++ book-app/book-api/src/controllers/authController.ts
@@ -1,4 +1,5 @@
 import { Request, Response } from 'express';
+import jwt from 'jsonwebtoken';
 import User from '../models/User.js';
 
 export const registerUser = async (req: Request, res: Response) => {
@@ -16,4 +17,46 @@ export const registerUser = async (req: Request, res: Response) => {
     const message = error instanceof Error ? error.message : 'Unknown error';
     res.status(500).json({ error: message });
   }
+};
+
+// To do:
+// Add bcrypt for password
+export const logIn = async (req: Request, res: Response) => {
+  const { username, password } = req.body;
+
+  if (!username || !password) {
+    res.status(401).json({ message: 'Username and password are required' });
+  }
+
+  try {
+    const user = await User.findOne({ username });
+
+    if (!user || user.password !== password) {
+      res.status(401).json({ message: 'Invalid username or password' });
+      return;
+    }
+
+    const accessToken = jwt.sign(
+      { id: user._id, username: user.username },
+      process.env.JWT_SECRET || '',
+      { expiresIn: '24h' }
+    );
+
+    res.cookie('accessToken', accessToken, {
+      httpOnly: true,
+      secure: false,
+      sameSite: 'none',
+      maxAge: 1000 * 60 * 60 * 24,
+    });
+
+    res.json({ message: 'You are logged in' });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ message: 'Internal server error' });
+  }
+};
+
+export const logOut = (_: Request, res: Response) => {
+  res.clearCookie('accessToken');
+  res.json({ message: 'You are logged out' });
 };
