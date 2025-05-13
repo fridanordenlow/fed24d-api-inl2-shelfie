b9f11481cb40d1927a1c45f805551024a46e2718
maria-jon <mariajonasson93@gmail.com>
2025-05-13 14:54:00 +0200

    add routing for BookListView and AddBookView

--- book-app/book-client/src/router/index.js
+++ book-app/book-client/src/router/index.js
@@ -1,5 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import HomeView from '@/views/HomeView.vue';
+import BookListView from '@/views/BookListView.vue'
+import AddBookView from '@/views/AddBookView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -8,6 +10,16 @@ const router = createRouter({
       path: '/',
       name: 'home',
       component: HomeView,
+    },
+    {
+      path: '/books',
+      name: 'Book list',
+      component: BookListView,
+    },
+    {
+      path: '/add-book',
+      name: 'Add new book',
+      component: AddBookView,
     }
   ],
 });
