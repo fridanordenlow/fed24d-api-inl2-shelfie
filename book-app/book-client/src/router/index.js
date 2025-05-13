e76f6f2ba64e0a27851aa71c92951d57c102ef97
Yuliia-fed23 <147343499+Yuliia-fed23@users.noreply.github.com>
2025-05-13 23:45:16 +0200

    created connection with DB. added bookId in Schema

--- book-app/book-client/src/router/index.js
+++ book-app/book-client/src/router/index.js
@@ -1,14 +1,20 @@
-import { createRouter, createWebHistory } from 'vue-router';
-import HomeView from '@/views/HomeView.vue';
+import { createRouter, createWebHistory } from "vue-router";
+import HomeView from "@/views/HomeView.vue";
+import BookDetail from "@/components/BookDetail.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: '/',
-      name: 'home',
+      path: "/",
+      name: "home",
       component: HomeView,
-    }
+    },
+    {
+      path: "/books/:id",
+      name: "book-detail",
+      component: BookDetail,
+    },
   ],
 });
 
