c7bd31f4f74fa8fa268b90058769d179ed8dae87
Frida Nordenlöw <frida.nordenlow@hotmail.com>
2025-05-14 15:38:04 +0200

    feat(admin-users): start working on admin panel for users

---
+++ book-app/book-client/src/views/AdminUsersView.vue
@@ -0,0 +1,15 @@
+<!-- I lösenordsskyddad adminpanel 
+ - Lista alla användare i tabellform 
+ - username, password, is_admin, created_at -->
+
+ <script setup>
+import { RouterLink } from 'vue-router';
+</script>
+
+<template>
+    <h1>Admin users</h1>
+    <RouterLink to="/">Home Page</RouterLink>
+</template>
+
+<style lang="scss" scoped>
+</style>
