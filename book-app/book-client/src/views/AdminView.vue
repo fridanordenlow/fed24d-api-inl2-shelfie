e16c56ad22d259e64f6d0fbd6e7ae8c146111fc2
maria-jon <mariajonasson93@gmail.com>
2025-05-16 12:08:15 +0200

    add AdminView

---
+++ book-app/book-client/src/views/AdminView.vue
@@ -0,0 +1,19 @@
+<script setup>
+import SiteHeader from "@/components/SiteHeader.vue";
+import { RouterLink } from "vue-router";
+
+</script>
+
+<template>
+    <SiteHeader title="Shelfie" />
+    <div class="wrapper">
+        <h2>Admin view</h2>
+        <RouterLink to="/">Back</RouterLink>
+
+        <div class="panel">
+            <RouterLink to="/admin-users-panel">View all users</RouterLink>
+            <RouterLink to="/book-table">View all books</RouterLink>
+            <RouterLink to="/add-book">Add new book</RouterLink>
+        </div>
+    </div>
+</template>
